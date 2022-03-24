53875968f0a36906f86cfa17e792ab1beff62795
Letícia Leal Moreira <lealmleticia@gmail.com>
2022-03-24 03:56:52 -0300

    Modified modals adminPage

--- src/modal/modal.js
+++ src/modal/modal.js
@@ -1,15 +1,29 @@
-class ModalProduct{
+class ModalProduct {
+    static divAddModal = document.querySelector(".modalAdd")
 
-    static newProduct(){
+
+    static newProduct() {
         const getButton = document.querySelector(".addProductCard")
-       
+        console.log(getButton)
         getButton.addEventListener("click", this.openModal.bind(this))
-    
     }
 
-    static openModal(){
-        const body = document.querySelector("body")
+    static editProduct() {
+        const getButtonEdited = document.querySelector(".editProduct")
+        console.log(ModalProduct.divAddModal)
+        console.log(getButtonEdited)
+        getButtonEdited.addEventListener("click", this.openModalEdit.bind(this))
+    }
 
+    static deleteProduct() {
+        const getButtonDelete = document.querySelector(".deleteProduct")
+
+        getButtonDelete.addEventListener("click", this.openModalDelete.bind(this))
+    }
+
+    static openModal() {
+
+        this.divAddModal.innerHTML = ""
         const div = document.createElement("div")
         const divBox = document.createElement("div")
         const divEdit = document.createElement("div")
@@ -23,15 +37,36 @@ class ModalProduct{
         const inputDescription = document.createElement("input")
         const labelThree = document.createElement("label")
         const divCategory = document.createElement("div")
-        const btnBread = document.createElement("button")
-        const btnFruits = document.createElement("button")
-        const btnDrinks = document.createElement("button")
+        divCategory.classList.add("category")
+        const radioBread = document.createElement("input")
+        radioBread.type = "radio"
+        radioBread.name = "categoria"
+        radioBread.id = "bread"
+        const labelBread = document.createElement("label")
+        labelBread.classList.add("legend")
+        labelBread.innerText = "Panificadora"
+        labelBread.setAttribute("for", "bread")
+        const radioFruits = document.createElement("input")
+        radioFruits.type = "radio"
+        radioFruits.name = "categoria"
+        radioFruits.id = "fruits"
+        const labelFruits = document.createElement("label")
+        labelFruits.classList.add("legend")
+        labelFruits.innerText = "Frutas"
+        labelFruits.setAttribute("for", "fruits")
+        const radioDrinks = document.createElement("input")
+        radioDrinks.type = "radio"
+        radioDrinks.name = "categoria"
+        radioDrinks.id = "drinks"
+        const labelDrinks = document.createElement("label")
+        labelDrinks.classList.add("legend")
+        labelDrinks.innerText = "Bebidas"
+        labelDrinks.setAttribute("for", "drinks")
         const labelFour = document.createElement("label")
         const inputPrice = document.createElement("input")
-        const labelFive= document.createElement("label")
+        const labelFive = document.createElement("label")
         const inputImage = document.createElement("input")
         const btnRegister = document.createElement("button")
-
         div.id = "modalEdit"
         div.classList.add("showModalEdit")
         divBox.classList.add("boxModalEdit")
@@ -41,33 +76,38 @@ class ModalProduct{
         divModal.classList.add("modalForm")
         form.classList.add("newProduct")
         form.action = "#"
+        spanStatus.innerText = "Cadastro de produto"
+        buttonExit.innerText = "X"
         labelOne.innerText = "Nome do Produto"
+        labelOne.classList.add("titleProduct")
         inputName.placeholder = "Digitar o nome"
         inputName.type = "text"
         inputName.name = "nome"
         labelTwo.innerText = "Descrição"
+        labelTwo.classList.add("subtitleProduct")
         inputDescription.placeholder = "Digitar a descrição"
         inputDescription.type = "text"
         inputDescription.name = "descricao"
-        labelThree.innerText = "categorias"
-        divCategory.classList.add("btnsCategory")
-        btnBread.classList.add("btnBread")
-        btnFruits.classList.add("btnFruits")
-        btnDrinks.classList.add("btnDrinks")
+        labelThree.innerText = "Categorias"
+        labelThree.classList.add("categoryLegend")
         labelFour.innerText = "Valor do Produto"
+        labelFour.classList.add("titlePrice")
         inputPrice.placeholder = "Digitar o valor aqui"
         inputPrice.type = "text"
         inputPrice.name = "preco"
         labelFive.innerText = "Link da imagem"
+        labelFive.classList.add("imageLink")
         inputImage.placeholder = "Inserir o link"
         inputImage.type = "url"
         inputImage.name = "imagem"
         btnRegister.classList.add("btnRegister")
-
-        divCategory.appendChild(btnBread)
-        divCategory.appendChild(btnFruits)
-        divCategory.appendChild(btnDrinks)
-
+        btnRegister.innerText = "Cadastrar Produto"
+        divCategory.appendChild(radioBread)
+        divCategory.appendChild(labelBread)
+        divCategory.appendChild(radioFruits)
+        divCategory.appendChild(labelFruits)
+        divCategory.appendChild(radioDrinks)
+        divCategory.appendChild(labelDrinks)
         form.appendChild(labelOne)
         form.appendChild(inputName)
         form.appendChild(labelTwo)
@@ -79,22 +119,205 @@ class ModalProduct{
         form.appendChild(labelFive)
         form.appendChild(inputImage)
         form.appendChild(btnRegister)
-
         divModal.appendChild(form)
-        
         divEdit.appendChild(spanStatus)
         divEdit.appendChild(buttonExit)
-
         divBox.appendChild(divEdit)
         divBox.appendChild(divModal)
-        
         div.appendChild(divBox)
-        body.appendChild(div)
-        
+        this.divAddModal.appendChild(div)
+
         buttonExit.addEventListener("click", () => {
+            div.classList.add("desaparecer")
+        })
+    }
+
+    static openModalEdit() {
+
+        this.divAddModal.innerHTML = ""
+        const div = document.createElement("div")
+        const divBox = document.createElement("div")
+        const divEdit = document.createElement("div")
+        const spanStatus = document.createElement("span")
+        const buttonExit = document.createElement("button")
+        const divModal = document.createElement("div")
+        const form = document.createElement("form")
+        const labelOne = document.createElement("label")
+        const inputName = document.createElement("input")
+        const labelTwo = document.createElement("label")
+        const inputDescription = document.createElement("input")
+        const labelThree = document.createElement("label")
+        const divCategory = document.createElement("div")
+        divCategory.classList.add("category")
+        const radioBread = document.createElement("input")
+        radioBread.type = "radio"
+        radioBread.name = "categoria"
+        radioBread.id = "bread"
+        const labelBread = document.createElement("label")
+        labelBread.classList.add("legend")
+        labelBread.innerText = "Panificadora"
+        labelBread.setAttribute("for", "bread")
+        const radioFruits = document.createElement("input")
+        radioFruits.type = "radio"
+        radioFruits.name = "categoria"
+        radioFruits.id = "fruits"
+        const labelFruits = document.createElement("label")
+        labelFruits.classList.add("legend")
+        labelFruits.innerText = "Frutas"
+        labelFruits.setAttribute("for", "fruits")
+        const radioDrinks = document.createElement("input")
+        radioDrinks.type = "radio"
+        radioDrinks.name = "categoria"
+        radioDrinks.id = "drinks"
+        const labelDrinks = document.createElement("label")
+        labelDrinks.classList.add("legend")
+        labelDrinks.innerText = "Bebidas"
+        labelDrinks.setAttribute("for", "drinks")
+        const labelFour = document.createElement("label")
+        const inputPrice = document.createElement("input")
+        const labelFive = document.createElement("label")
+        const inputImage = document.createElement("input")
+        const divBtns = document.createElement("div")
+        const btnRegister = document.createElement("button")
+        const btnLeave = document.createElement("button")
+        div.id = "modalEdit"
+        div.classList.add("showModalEdit")
+        divBox.classList.add("boxModalEdit")
+        divEdit.classList.add("edit")
+        spanStatus.classList.add("statusSpan")
+        buttonExit.classList.add("btnExit")
+        divModal.classList.add("modalForm")
+        form.classList.add("newProduct")
+        form.action = "#"
+        spanStatus.innerText = "Edição de produto"
+        buttonExit.innerText = "X"
+        labelOne.innerText = "Nome do Produto"
+        labelOne.classList.add("titleProduct")
+        inputName.placeholder = ""
+        inputName.type = "text"
+        inputName.name = "nome"
+        labelTwo.innerText = "Descrição"
+        labelTwo.classList.add("subtitleProduct")
+        inputDescription.placeholder = ""
+        inputDescription.type = "text"
+        inputDescription.name = "descricao"
+        labelThree.innerText = "Categorias"
+        labelThree.classList.add("categoryLegend")
+        labelFour.innerText = "Valor do Produto"
+        labelFour.classList.add("titlePrice")
+        inputPrice.placeholder = ""
+        inputPrice.type = "text"
+        inputPrice.name = "preco"
+        labelFive.innerText = "Link da imagem"
+        labelFive.classList.add("imageLink")
+        inputImage.placeholder = ""
+        inputImage.type = "url"
+        inputImage.name = "imagem"
+        btnLeave.classList.add("btnLeave")
+        btnLeave.innerText = "Excluir"
+        btnRegister.classList.add("btnRegister")
+        btnRegister.innerText = "Salvar alterações"
+        divCategory.appendChild(radioBread)
+        divCategory.appendChild(labelBread)
+        divCategory.appendChild(radioFruits)
+        divCategory.appendChild(labelFruits)
+        divCategory.appendChild(radioDrinks)
+        divCategory.appendChild(labelDrinks)
+
+        divBtns.appendChild(btnLeave)
+        divBtns.appendChild(btnRegister)
+
+        form.appendChild(labelOne)
+        form.appendChild(inputName)
+        form.appendChild(labelTwo)
+        form.appendChild(inputDescription)
+        form.appendChild(labelThree)
+        form.appendChild(divCategory)
+        form.appendChild(labelFour)
+        form.appendChild(inputPrice)
+        form.appendChild(labelFive)
+        form.appendChild(inputImage)
+        form.appendChild(divBtns)
+        divModal.appendChild(form)
+        divEdit.appendChild(spanStatus)
+        divEdit.appendChild(buttonExit)
+        divBox.appendChild(divEdit)
+        divBox.appendChild(divModal)
+        div.appendChild(divBox)
+        this.divAddModal.appendChild(div)
+
+        buttonExit.addEventListener("click", () => {
+            div.classList.add("desaparecer")
+        })
+
+        // btnLeave.addEventListener("click", () => {
+        //     EXCLUIR O CADASTRO
+        // })
+
+        // btnRegister.addEventListener("click", () => {
+        //     SALVAR A ALTERAÇÃO
+        // })
+    }
+
+    static openModalDelete() {
+        this.divAddModal.innerHTML = ""
+
+        const div = document.createElement("div")
+        const divBox = document.createElement("div")
+        const divDelete = document.createElement("div")
+        const spanStatus = document.createElement("span")
+        const buttonExit = document.createElement("button")
+        const divStatus = document.createElement("div")
+        const spanMessage = document.createElement("span")
+        const divBtns = document.createElement("div")
+        const btnYes = document.createElement("button")
+        const btnNo = document.createElement("button")
+
+
+        div.id = "modalDelete"
+        div.classList.add("showModalDelete")
+        divBox.classList.add("boxModalDelete")
+        divDelete.classList.add("delete")
+        spanStatus.classList.add("statusSpan")
+        spanStatus.innerText = "Exclusão de Produto"
+        buttonExit.classList.add("btnExit")
+        buttonExit.innerText = "X"
+        divStatus.classList.add("statusOver")
+        spanMessage.classList.add("messageStatus")
+        spanMessage.innerText = "Tem certeza que deseja excluir este produto?"
+        divBtns.classList.add("divButtons")
+        btnYes.classList.add("yes")
+        btnYes.innerText = "Sim"
+        btnNo.classList.add("no")
+        btnNo.innerText = "Não"
+
+
+        divDelete.appendChild(spanStatus)
+        divDelete.appendChild(buttonExit)
+
+        divStatus.appendChild(spanMessage)
+        divStatus.appendChild(btnYes)
+        divStatus.appendChild(btnNo)
+
+        divBox.appendChild(divDelete)
+        divBox.appendChild(divStatus)
+
+        div.appendChild(divBox)
+
+        this.divAddModal.appendChild(div)
+
+        buttonExit.addEventListener("click", () => {
+            div.classList.add("desaparecer")
+        })
+
+        // btnYes.addEventListener("click", () => {
+        //     VAI APAGAR O PRODUTO
+        // })
+
+        btnNo.addEventListener("click", () => {
             div.classList.add("desaparecer")
         })
     }
 }
 
-export {ModalProduct}
+export { ModalProduct }
